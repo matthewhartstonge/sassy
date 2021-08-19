c8c2315e1ae295d64c100f8b235a0a196b18091e
Matthew Hartstonge <matt@mykro.co.nz>
2021-08-20 09:56:50 +1200

    :sparkles: sassy/sas/protocols: adds support for protocols.

---
+++ sas/protocols/protocols.go
@@ -0,0 +1,105 @@
+package protocols
+
+import (
+	// Standard Library Imports
+	"net/url"
+	"strings"
+)
+
+type Protocol string
+
+const (
+	HTTP  Protocol = "http"
+	HTTPS Protocol = "https"
+)
+
+const (
+	numProtocols = 2
+	paramKey     = "spr"
+)
+
+type Protocols struct {
+	// hasValues tracks whether protocols have been added.
+	hasValues bool
+	// protocols must be in the order https,http
+	protocols [numProtocols]Protocol
+}
+
+func defaultProtocols() Protocols {
+	return Protocols{
+		hasValues: true,
+		protocols: [numProtocols]Protocol{HTTPS, HTTP},
+	}
+}
+
+func (r Protocols) ToString() string {
+	var out []string
+	for _, protocol := range r.protocols {
+		if protocol != "" {
+			out = append(out, string(protocol))
+		}
+	}
+
+	return strings.Join(out, ",")
+}
+
+func (r Protocols) SetParam(params *url.Values) {
+	if r.hasValues {
+		params.Add(paramKey, r.ToString())
+	}
+}
+
+func (r Protocols) GetParam() (protocols string) {
+	if r.hasValues {
+		values := &url.Values{}
+		r.SetParam(values)
+
+		protocols = values.Encode()
+	}
+
+	return
+}
+
+func (r Protocols) GetURLDecodedParam() (protocols string) {
+	if r.hasValues {
+		protocols, _ = url.QueryUnescape(r.GetParam())
+	}
+
+	return
+}
+
+// Parse returns a valid set of protocols. Possible values are both HTTPS and
+// HTTP (https,http) or HTTPS only (https).
+// The default value is https,http.
+func Parse(protocols string) (spr Protocols) {
+	spr = Protocols{
+		hasValues: false,
+		protocols: [numProtocols]Protocol{},
+	}
+
+	splitProtocols := strings.Split(protocols, ",")
+	if len(splitProtocols) == 1 && string(HTTP) == strings.ToLower(splitProtocols[0]) {
+		// Note that HTTP only is not a permitted value.
+		return defaultProtocols()
+	}
+
+	sprMap := protocolMap()
+	for _, protocol := range splitProtocols {
+		check := Protocol(strings.ToLower(protocol))
+		if protocolIndex, ok := sprMap[check]; ok {
+			spr.protocols[protocolIndex] = check
+			if !spr.hasValues {
+				spr.hasValues = true
+			}
+		}
+	}
+
+	return spr
+}
+
+func protocolMap() map[Protocol]int {
+	return map[Protocol]int{
+		HTTPS: 0,
+		HTTP:  1,
+	}
+}
