718a9df7919cc5bda4335ee8ee5ed6173775ed3a
Matthew Hartstonge <matt@mykro.co.nz>
2021-10-20 23:17:44 +1300

    :truck: storage/ips: renames package `signedip` to `ips` for consistency.

--- storage/ips/ips.go
+++ storage/ips/ips.go
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package signedip
+package ips
 
 import (
 	// Standard Library Imports
--- storage/ips/ips_test.go
+++ storage/ips/ips_test.go
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package signedip
+package ips
 
 import (
 	"testing"
