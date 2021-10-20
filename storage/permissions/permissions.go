c40ce21e7ce82e916c8730b686ab762ede1783dd
Matthew Hartstonge <matt@mykro.co.nz>
2021-10-20 23:59:25 +1300

    :children_crossing: storage/permissions: adds an index generating closure, so we don't have to manually track indices in `signedPermissionMap()`.

--- storage/permissions/permissions.go
+++ storage/permissions/permissions.go
@@ -131,84 +131,92 @@ type signedPermissionSpec struct {
 }
 
 func signedPermissionMap() map[SignedPermission]signedPermissionSpec {
+	// nextIndex provides an index generating closure, so we don't have to
+	// manually track indices in the map.
+	i := -1
+	nextIndex := func() int {
+		i++
+		return i
+	}
+
 	// Refer: https://docs.microsoft.com/en-us/rest/api/storageservices/create-service-sas#permissions-for-a-directory-container-or-blob
 	return map[SignedPermission]signedPermissionSpec{
 		Read: {
 			OpName:        "Read",
 			OpDescription: "Read the content, block list, properties, and metadata of any blob in the container or directory. Use a blob as the source of a copy operation.",
-			Index:         0,
+			Index:         nextIndex(),
 			APIVersion:    versions.VAll,
 		},
 		Add: {
 			OpName:        "Add",
 			OpDescription: "Add a block to an append blob.",
-			Index:         1,
+			Index:         nextIndex(),
 			APIVersion:    versions.VAll,
 		},
 		Create: {
 			OpName:        "Create",
 			OpDescription: "Write a new blob, snapshot a blob, or copy a blob to a new blob.",
-			Index:         2,
+			Index:         nextIndex(),
 			APIVersion:    versions.VAll,
 		},
 		Write: {
 			OpName:        "Write",
 			OpDescription: "Create or write content, properties, metadata, or block list. Snapshot or lease the blob. Resize the blob (page blob only). Use the blob as the destination of a copy operation.",
-			Index:         3,
+			Index:         nextIndex(),
 			APIVersion:    versions.VAll,
 		},
 		Delete: {
 			OpName:        "Delete",
 			OpDescription: "Delete a blob. For version 2017-07-29 and later, the Delete permission also allows breaking a lease on a blob. For more information, see the Lease Blob operation.",
-			Index:         4,
+			Index:         nextIndex(),
 			APIVersion:    versions.VAll,
 		},
 		DeleteVersion: {
 			OpName:        "Delete version",
 			OpDescription: "Delete a blob version.",
-			Index:         5,
+			Index:         nextIndex(),
 			APIVersion:    versions.V20191212,
 		},
 		PermanentDelete: {
 			OpName:        "Permanent delete",
 			OpDescription: "Permanently delete a blob snapshot or version.",
-			Index:         6,
+			Index:         nextIndex(),
 			APIVersion:    versions.V20200210,
 		},
 		List: {
 			OpName:        "List",
 			OpDescription: "List blobs non-recursively.",
-			Index:         7,
+			Index:         nextIndex(),
 			APIVersion:    versions.VAll,
 		},
 		Tags: {
 			OpName:        "Tags",
 			OpDescription: "Read or write the tags on a blob.",
-			Index:         8,
+			Index:         nextIndex(),
 			APIVersion:    versions.V20191212,
 		},
 		Move: {
 			OpName:        "Move",
 			OpDescription: "Move a blob or a directory and its contents to a new location. This operation can optionally be restricted to the owner of the child blob, directory, or parent directory if the `saoid` parameter is included on the SAS token and the sticky bit is set on the parent directory.",
-			Index:         9,
+			Index:         nextIndex(),
 			APIVersion:    versions.V20200210,
 		},
 		Execute: {
 			OpName:        "Execute",
 			OpDescription: "Get the system properties and, if the hierarchical namespace is enabled for the storage account, get the POSIX ACL of a blob. If the hierarchical namespace is enabled and the caller is the owner of a blob, this permission grants the ability to set the owning group, POSIX permissions, and POSIX ACL of the blob. Does not permit the caller to read user-defined metadata.",
-			Index:         10,
+			Index:         nextIndex(),
 			APIVersion:    versions.V20200210,
 		},
 		Ownership: {
 			OpName:        "Ownership",
 			OpDescription: "When the hierarchical namespace is enabled, this permission enables the caller to set the owner or the owning group, or to act as the owner when renaming or deleting a directory or blob within a directory that has the sticky bit set.",
-			Index:         11,
+			Index:         nextIndex(),
 			APIVersion:    versions.V20200210,
 		},
 		Permissions: {
 			OpName:        "Permissions",
 			OpDescription: "When the hierarchical namespace is enabled, this permission allows the caller to set permissions and POSIX ACLs on directories and blobs.",
-			Index:         12,
+			Index:         nextIndex(),
 			APIVersion:    versions.V20200210,
 		},
 	}
